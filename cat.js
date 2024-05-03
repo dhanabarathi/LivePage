ac4578439d525bab205d80f0a344e33be4ea27df
dhanabarathi <166902526+dhanabarathi@users.noreply.github.com>
2024-05-03 12:02:52 +0530

    Add files via upload

---
+++ cat.js
@@ -0,0 +1,8 @@
+function boxlighton(){  
+  document.getElementById("onbulb").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+  document.getElementById("eyecat").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+ }
+ function boxlightoff(){
+   document.getElementById("onbulb").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+   document.getElementById("eyecat").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+ }
