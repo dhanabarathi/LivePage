ccad81665f14076e17ec0a8df68366eb4958e358
dhanabarathi <166902526+dhanabarathi@users.noreply.github.com>
2024-05-05 21:08:19 +0530

    Add files via upload

---
+++ traffic.js
@@ -0,0 +1,20 @@
+function red(){
+  document.getElementById("redbulb").style.background="red"
+  document.getElementById("yellowbulb").style.background=""
+  document.getElementById("greenbulb").style.background=""
+
+ 
+}
+function yellow(){
+    document.getElementById("yellowbulb").style.background="yellow"
+    document.getElementById("redbulb").style.background=""
+    document.getElementById("greenbulb").style.background=""
+
+}
+function green(){
+    document.getElementById("greenbulb").style.background="green"
+    document.getElementById("redbulb").style.background=""
+    document.getElementById("yellowbulb").style.background=""
+
+
+}
